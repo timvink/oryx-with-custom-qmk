d702e4f021b2d00757e99b0d761abb0a43ac5dfb
Tim Vink <vinktim@gmail.com>
2025-04-14 10:44:24 +0200

    Update `keymap.c`, right thumb button to be included in chordal hold

--- bZ5m5/keymap.c
+++ bZ5m5/keymap.c
@@ -216,5 +216,5 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
   'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 
   'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 
   'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 
-  '*', '*', '*', '*'
+  '*', '*', '*', 'R'
     );
